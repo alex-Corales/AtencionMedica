9fd0ca807075fbc54bf2b87a2e86372d2c3ed924
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-10-28 09:12:48 -0300

    Agrego antecedentes patologicos (vista)

--- public/scripts/consulta.js
+++ public/scripts/consulta.js
@@ -1,11 +1,11 @@
-let contadorConsulta = 0;
-
 function agregarFilaConsulta() {
+    let contadorConsulta = 0;
+
     const descripcion = document.getElementById('inputDescripcion').value;
     const estado = document.getElementById('selectEstado').value;
 
     if (!descripcion || !estado) {
-        alert("Por favor, complete la descripción y seleccione un estado.");
+        alert('Tiene que llenar todos los campos');
         return;
     }
 
@@ -27,7 +27,69 @@ function agregarFilaConsulta() {
 
     document.getElementById('tablaDiagnosticos').querySelector('tbody').appendChild(fila);
 
-    // Limpiar los campos después de agregar
     document.getElementById('inputDescripcion').value = '';
     document.getElementById('selectEstado').value = '';
+}
+
+
+function agregarFilaAlergia() {
+    const nombreAlergia = document.getElementById('inputNombreAlergia').value;
+    const fechaAlergia = document.getElementById('fechaAlergia').value;
+    const importanciaAlergia = document.getElementById('selectImportancia').value;
+
+    if (!nombreAlergia || !fechaAlergia || !importanciaAlergia) {
+        alert('Tiene que llenar todos los campos');
+        return;
+    }
+
+    const fila = document.createElement('tr'); 
+
+    const columnaNombreAlergia = document.createElement('td');
+    columnaNombreAlergia.textContent = nombreAlergia;
+
+    const columnaFechaAlergia = document.createElement('td');
+    columnaFechaAlergia.textContent = fechaAlergia;
+
+    const columnaImportanciaAlergia = document.createElement('td');
+    columnaImportanciaAlergia.textContent = importanciaAlergia;
+
+    fila.appendChild(columnaNombreAlergia);
+    fila.appendChild(columnaFechaAlergia);
+    fila.appendChild(columnaImportanciaAlergia);
+
+    document.getElementById('tablaAlergias').querySelector('tbody').appendChild(fila); 
+
+    document.getElementById('inputNombreAlergia').value = '';
+    document.getElementById('fechaAlergia').value = '';
+    document.getElementById('selectImportancia').value = '';
+}
+
+function agregarFilaAntecedentePatologico() {
+    const descripcionAntecedente = document.getElementById('inputDescripcionAntecedente').value;
+    const fechaDesde = document.getElementById('fechaDesde').value;
+    const fechaHasta = document.getElementById('fechaHasta').value;
+
+    if (!descripcionAntecedente || !fechaDesde || !fechaHasta) {
+        alert('Tiene que llenar todos los campos');
+        return;
+    }
+
+    const filaAntecedentes = document.createElement('tr');
+
+    const columnaDescripcion = document.createElement('td');
+    columnaDescripcion.textContent = descripcionAntecedente;
+    const columnaFechaDesde = document.createElement('td');
+    columnaFechaDesde.textContent = fechaDesde;
+    const columnaFechaHasta = document.createElement('td');
+    columnaFechaHasta.textContent = fechaHasta;
+
+    filaAntecedentes.appendChild(columnaDescripcion);
+    filaAntecedentes.appendChild(columnaFechaDesde);
+    filaAntecedentes.appendChild(columnaFechaHasta);
+
+    document.getElementById('tablaAntecedentes').querySelector('tbody').appendChild(filaAntecedentes);
+
+    document.getElementById('inputDescripcionAntecedente').value = '';
+    document.getElementById('fechaDesde').value = '';
+    document.getElementById('fechaHasta').value = '';
 }
