f67c3eb998bda8ed909858654595bab713c96a1f
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-07 23:28:36 -0300

    commit

--- connection/connectionBD.js
+++ connection/connectionBD.js
@@ -5,20 +5,18 @@ let connection;
 async function connectDB() {
     if (!connection) {
         try {
-            const DEFAULT_CONFIG = {
-                host: 'localhost',
-                user: 'root',
-                password: '',
-                database: 'atencionMedica'
+            const connectionConfig = {
+                host: process.env.DB_HOST || 'localhost',
+                user: process.env.DB_USER || 'root',
+                password: process.env.DB_PASSWORD || '',
+                database: process.env.DB_NAME || 'atencionMedica'
             };
-
-            const connectionConfig = process.env.DATABASE_URL || DEFAULT_CONFIG;
 
             connection = await mysql.createConnection(connectionConfig);
             console.log('Conectado a la base de datos');
         } catch (err) {
             console.log('Error al conectar a la base de datos:', err);
-            throw err;  // Lanzar error si no se puede conectar
+            throw err; // Lanza un error si no se puede conectar
         }
     }
     return connection;
