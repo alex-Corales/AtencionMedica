cc77475aaf702a8ca9ae8d9903fadff1cf0612da
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-09 21:51:10 -0300

    commit

--- routes/paciente.js
+++ routes/paciente.js
@@ -8,7 +8,16 @@ export const crearPacienteRouter = ({ pacienteModel }) => {
     const pacienteController = new PacienteController({ pacienteModel });
 
     pacienteRouter.get('/', pacienteController.paciente);
-    pacienteRouter.post('/dni', pacienteController.obtenerPacientesPorDNI); 
+    pacienteRouter.post('/dni', pacienteController.obtenerPacientesPorDNI);
+    pacienteRouter.get('/pacienteAgenda', async (req, res) => {
+        const { id_paciente } = req.query;
+
+        const pacientes = await pacienteModel.obtenerPacientePorId(id_paciente);
+
+        console.log(id_paciente);
+
+        res.render('paciente/pacienteAgenda', { pacientes });
+    })
 
     return pacienteRouter;
 };
